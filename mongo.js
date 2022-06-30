e3150af34fc18d660d3e8307ef8763b6f36e897b
Styliani Tsovou <stellatsovou@gmail.com>
2022-06-30 23:20:17 +0300

    ex3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.0wktqwo.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Phonebook', phonebookSchema)
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected')
+
+    if (process.argv.length === 3) {
+        Person.find({}).then(result => {
+            result.forEach(person => {
+                console.log(person)
+            })
+            return mongoose.connection.close()
+        })
+    } else if (process.argv.length === 5){
+        const person = new Person({
+            name: process.argv[3],            
+            number: process.argv[4]
+        })
+        return person.save().then(() => {
+            console.log(`added ${person.name} number ${person.number} to phonebook`)
+            return mongoose.connection.close()
+        })        
+    }  
+  })
+  .catch((err) => console.log(err))
