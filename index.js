81567b8448aa82b4fffa52cde471379bcc154f91
stellatsv <stellatsovou@gmail.com>
2023-04-03 22:39:18 +0300

    ex3.15

--- index.js
+++ index.js
@@ -3,28 +3,6 @@ const express = require('express')
 const app = express()
 
 const Person = require('./models/person')
-// let persons = [ 
-//     { 
-//         "id": 1,
-//         "name": "Arto Hellas", 
-//         "number": "040-123456"
-//       },
-//       { 
-//         "id": 2,
-//         "name": "Ada Lovelace", 
-//         "number": "39-44-5323523"
-//       },
-//       { 
-//         "id": 3,
-//         "name": "Dan Abramov", 
-//         "number": "12-43-234345"
-//       },
-//       { 
-//         "id": 4,
-//         "name": "Mary Poppendieck", 
-//         "number": "39-23-6423122"
-//       }
-// ]
 
 app.use(express.static('build'))
 app.use(require('cors')());
@@ -54,7 +32,6 @@ app.get('/info', (request, response) => {
 })
   
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
     const person = Person.findById(request.params.id).then(p => {
       if (p) {    
         response.json(p)  
@@ -64,28 +41,31 @@ app.get('/api/persons/:id', (request, response) => {
       }
     }).catch(error => {     
       console.log(error)      
-      response.status(500).end()    
+      response.status(400).send({ error: 'malformatted id' })  
     })
     
 })
+ 
+
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)  
-    response.status(204).end()
+    const person =  Person.findByIdAndRemove(request.params.id).then(result => {
+      if (result) {    
+        response.json(result)  
+      } 
+      else {    
+        response.status(404).end()  
+      }
+    }).catch(error => {     
+      console.log(error)      
+      response.status(400).send({ error: 'malformatted id' })  
+    })
 })
 
 const generateId = () => {
    return Math.floor(Math.random() * 100)
 }
-  
-const isFound = (name) => persons.some(person => {
-    if (person.name === name) {
-      return true;
-    }
 
-    return false;
-});
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -95,12 +75,6 @@ app.post('/api/persons', (request, response) => {
             error: 'details missing' 
         })
     }
-
-    // if (isFound(body.name)) {
-    //     return response.status(400).json({ 
-    //         error: 'name must be unique' 
-    //     })
-    // }
 
     const person = new Person({
         name: body.name,
